8dc6c9cd4df40fa316775ff8f33458e96fa0af3e
Faizan Ali <fa588@drexel.edu>
2025-04-18 03:16:08 +0000

    mySQL code for database construction

---
+++ CI103.session.sql
@@ -0,0 +1,48 @@
+-- Budgeting App Database --
+
+DROP TABLE IF EXISTS Income, Expenses, Categories, Users;
+
+create table Users(
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(25) NOT NULL,
+    last_name VARCHAR(25) NOT NULL,
+    user_name VARCHAR(35) NOT NULL UNIQUE,
+    email VARCHAR(35) NOT NULL UNIQUE,
+    pass_code VARCHAR(35) NOT NULL UNIQUE
+);
+
+create table Categories(
+    category_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    category_name VARCHAR(50),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+
+create table Expenses(
+    expense_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    category_id INT,
+    expense_amount DECIMAL(5, 2) NOT NULL,
+    expense_date DATE NOT NULL,
+    expense_info VARCHAR(100),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (category_id) REFERENCES Categories(category_id)
+);
+
+create table Income(
+    income_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    category_id INT,
+    income_amount DECIMAL(5, 2) NOT NULL,
+    income_date DATE NOT NULL,
+    income_info VARCHAR(100),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (category_id) REFERENCES Categories(category_id)
+);
+
+
+
+
+
+
